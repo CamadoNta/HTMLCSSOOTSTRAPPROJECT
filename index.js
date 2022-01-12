b1ca14279dced3edeeb095718c51e8c8dad8a1cd
Mouhrach Choaib <81709206+Choaib541@users.noreply.github.com>
2022-01-12 01:33:28 +0000

    main

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("open").onclick = function () {
-  document.getElementById("list").style.margin = "0";
-};
-document.getElementById("close").onclick = function () {
-  document.getElementById("list").style.marginLeft = "-200%";
-};
